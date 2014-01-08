34606c560f235f4aa71e273ee06e263a71aa6980
JohnToCoder <zh.wolf5@gmail.com>
2014-01-08 23:33:29 +0800

    010800

--- CMG/JS/index.js
+++ CMG/JS/index.js
@@ -5,24 +5,25 @@ $(document).ready(function () {
 var strtxt = "";
 function InitDataGrid() {
     $('#tabNote').datagrid({
-        fit: true, //自动大小 
+        //fit: true, //自动大小 
         //        url:'JS/data.json',   
         rownumbers: true, //行号   
         //loadMsg:'数据装载中......',    
         singleSelect: true, //单行选取  
         pagination: false, //显示分页 
         columns: [[
-        { field: 'ID', title: 'ID', hidden: true, width: 0 },
-        { field: 'Note', title: 'Note', hidden: true, width: 0 },
-        { field: 'CreatDate', title: 'date', hidden: true, width: 0 },
-        { field: 'Creator', title: 'Creator', hidden: true, width: 0 },
-        { field: 'Title', title: '通知', align: 'left', width: 300,
+        { field: 'ID', title: 'ID', hidden: true, width: 0 },       
+        { field: 'Title', title: '通知', align: 'left', width: 280,
             formatter: function (value, row, index) {
                 strtxt = row.ID + '|' + row.Title + '|' + row.Note + '|' + row.CreatDate + '|' + row.Creator;
                 //                alert(strtxt);
                 return "<a href=\"javascript:void(0)\" onclick=\"titClick(" + row.ID + ")\">" + row.Title + "</a>";
             }
-        }]],
+        },
+        { field: 'Note', title: 'Note', hidden: true, width: 0 },
+        { field: 'CreatDate', title: 'date', hidden: true, width: 0 },
+        { field: 'Creator', title: 'Creator', hidden: true, width: 0 } 
+        ]],
         fitColumns: true
     });
     $.post('ashx/getNote.ashx',
